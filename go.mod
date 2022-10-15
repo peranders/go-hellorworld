dda34af018508ffd1244a6741c83980572b64537
peraj <peraj@google.com>
2022-10-16 00:10:01 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/GoogleCloudPlatform/golang-samples/run/helloworld
+
+go 1.13
+
+require google.golang.org/api v0.98.0 // indirect
---
+++ main.go
@@ -0,0 +1,85 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START cloudrun_helloworld_service]
+// [START run_helloworld_service]
+
+// Sample run-helloworld is a minimal Cloud Run service.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"context"
+	"io"
+	"google.golang.org/api/idtoken"
+
+)
+
+func main() {
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World3"
+	}
+	
+	iapHeader := r.Header.Get("X-Goog-IAP-JWT-Assertion")
+	if err := validateJWTFromComputeEngine(w, iapHeader); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, "Hello %s!\n", name)
+	//fmt.Fprintf(w, "Header = %s", iapHeader)
+}
+
+
+// validateJWTFromComputeEngine validates a JWT found in the
+// "x-goog-iap-jwt-assertion" header.
+func validateJWTFromComputeEngine(w io.Writer, iapJWT string) error {
+// 	// iapJWT := "YmFzZQ==.ZW5jb2RlZA==.and0" // req.Header.Get("X-Goog-IAP-JWT-Assertion")
+	//iapJWT := req.Header.Get("X-Goog-IAP-JWT-Assertion")
+
+ 	ctx := context.Background()
+// 	aud := fmt.Sprintf("/projects/%s/global/backendServices/%s", projectNumber, backendServiceID)
+	aud := "/projects/940733461091/global/backendServices/3463990049096674786"
+ 	payload, err := idtoken.Validate(ctx, iapJWT, aud)
+ 	if err != nil {
+ 			return fmt.Errorf("idtoken.Validate: %v", err)
+ 	}
+
+ 	// payload contains the JWT claims for further inspection or validation
+ 	fmt.Fprintf(w, "payload: %v \n", payload)
+
+ 	return nil
+ }
+
